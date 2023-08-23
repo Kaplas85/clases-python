e52ea9990baef0086bb9f0d99286ab56ecf04b7b
Cesar <cesarbaudi1@gmail.com>
2023-08-22 21:19:14 -0400

    Reset class

--- arithmetic_operators.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Introduce valor de X: "))
-y = int(input("Introduce valor de Y: "))
-
-print("La suma es: ", x + y)
-print("La resta es: ", x - y)
-print("La multiplicación es: ", x * y)
-print("La división es: ", x / y)
-print("El residuo es: ", x % y)
-print("El exponencial es: ", x ** y)
--- tuples.py
+++
@@ -1,9 +0,0 @@
-# market = ("mantequilla", "papas", "harina")
-# print(market)
-
-# my_custom_tuple = ("manzanas",)
-# print(my_custom_tuple, type(my_custom_tuple))
-
-first_name, last_name = ("Cesar", "Baudi")
-
-print(first_name, last_name)
